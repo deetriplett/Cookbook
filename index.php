a08a65b6071e4e28caf98d05af5d8caff2a70764
deetriplett <deetriplett@gmail.com>
2016-05-12 15:34:59 -0400

    Initial Commit, Index.php and first style sheet.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset=utf-8>
+  	<title>Dawn Monroe's Resume</title>
+  	<link href="css/style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <section class="sidebar text-center">
+      <div class="avatar">
+        <img src="">
+      </div>
+      <h1>Dawn Monroe</h1>
+      <p>Louisville, KY</p>
+      <hr />
+      <p>Welcome to my Resume!</p>
+      <hr />
+      <ul class="social">
+        <li><a href="twitter.com/deetriplett"><span class="icon twitter"></span></a></li>
+      </ul>
+    </section>
+    <section class="main">
+      <p>Let's Get Started!</p>
+      <p><?php echo "Hello, World!"?></p>
+    </section>
+  </body>
+</html>
