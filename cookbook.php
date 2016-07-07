f4c74e0f2a3b0db73950331db8cfc07466367b38
deetriplett <deetriplett@gmail.com>
2016-07-07 16:48:00 -0700

    added yield to recipe1

--- cookbook.php
+++ cookbook.php
@@ -3,14 +3,14 @@
 include "classes/recipes.php";
 
 $recipe1 = new Recipe();
-//$recipe1->source = "Memaw Fowler";
+$recipe1->setSource = "Memaw Fowler";
 $recipe1->setTitle("lemon bars");
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour", 2, "cup");
 
 
 $recipe2 = new Recipe();
-//$recipe2->source = "Betty Crocker";
+$recipe2->setSource = "Betty Crocker";
 $recipe2->setTitle("chocolate mud cake");
 
 echo $recipe1->getTitle();
@@ -27,6 +27,10 @@ $recipe1->addTag("lemon");
 $recipe1->addTag("dessert");
 
 echo implode(", ", $recipe!->getTags());
+
+$recipe1->setYield("6 servings");
+$recipe1->getYield();
+$recipe1->getSource();
 
 
 echo $recipe1->displayRecipe();
