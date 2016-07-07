e79b8d0f4c4b28020d64433645c3427cd20c9a4f
deetriplett <deetriplett@gmail.com>
2016-07-07 16:38:09 -0700

    Added tags to $recipe1

--- cookbook.php
+++ cookbook.php
@@ -3,14 +3,14 @@
 include "classes/recipes.php";
 
 $recipe1 = new Recipe();
-$recipe1->source = "Memaw Fowler";
+//$recipe1->source = "Memaw Fowler";
 $recipe1->setTitle("lemon bars");
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour", 2, "cup");
 
 
 $recipe2 = new Recipe();
-$recipe2->source = "Betty Crocker";
+//$recipe2->source = "Betty Crocker";
 $recipe2->setTitle("chocolate mud cake");
 
 echo $recipe1->getTitle();
@@ -22,6 +22,12 @@ $recipe1->addInstruction("Whisk eggs");
 $recipe1->addInstruction("Fold in flour");
 
 echo implode("\n", $recipe1->getInstructions());
+
+$recipe1->addTag("lemon");
+$recipe1->addTag("dessert");
+
+echo implode(", ", $recipe!->getTags());
+
 
 echo $recipe1->displayRecipe();
 echo $recipe2->displayRecipe(); 
