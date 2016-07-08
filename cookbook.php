1ad030aa45ff79faaca6da0a4be0c9f76bfdeda0
deetriplett <deetriplett@gmail.com>
2016-07-08 08:31:31 -0700

    added allrecipes inc

--- cookbook.php
+++ cookbook.php
@@ -1,24 +1,7 @@
 <?php
 
 include "classes/recipes.php";
-include "classes/render.php;
+include "classes/render.php";
+include "inc/allrecipes.php";
 
-$recipe1 = new Recipe("lemon bars");
-$recipe1->setSource = "Memaw";
-$recipe1->addIngredient("egg", 1);
-$recipe1->addIngredient("flour", 2, "cup");
-
-
-$recipe2 = new Recipe("chocolate mud cake");
-$recipe2->setSource = "Betty Crocker";
-
-$recipe1->addInstruction("Whisk eggs");
-$recipe1->addInstruction("Fold in flour");
-
-
-$recipe1->addTag("lemon");
-$recipe1->addTag("dessert");
-
-$recipe1->setYield("6 servings");
-
-echo Render::displayRecipe($recipe1);
+echo Render::displayRecipe($belgian_waffles);
