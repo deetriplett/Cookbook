07d862d922e29b843be4231a1c41a5c8ebccceec
deetriplett <deetriplett@gmail.com>
2016-07-08 06:10:00 -0700

    Update cookbook.php

--- cookbook.php
+++ cookbook.php
@@ -1,37 +1,24 @@
 <?php
 
 include "classes/recipes.php";
+include "classes/render.php;
 
-$recipe1 = new Recipe();
-$recipe1->setSource = "Memaw Fowler";
-$recipe1->setTitle("lemon bars");
+$recipe1 = new Recipe("lemon bars");
+$recipe1->setSource = "Memaw";
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour", 2, "cup");
 
 
-$recipe2 = new Recipe();
+$recipe2 = new Recipe("chocolate mud cake");
 $recipe2->setSource = "Betty Crocker";
-$recipe2->setTitle("chocolate mud cake");
-
-echo $recipe1->getTitle();
-foreeach($recipe1->getIngredients90 as $ing) {
-  echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
-}
 
 $recipe1->addInstruction("Whisk eggs");
 $recipe1->addInstruction("Fold in flour");
 
-echo implode("\n", $recipe1->getInstructions());
 
 $recipe1->addTag("lemon");
 $recipe1->addTag("dessert");
 
-echo implode(", ", $recipe!->getTags());
-
 $recipe1->setYield("6 servings");
-$recipe1->getYield();
-$recipe1->getSource();
 
-
-echo $recipe1->displayRecipe();
-echo $recipe2->displayRecipe(); 
+echo Render::displayRecipe($recipe1);
