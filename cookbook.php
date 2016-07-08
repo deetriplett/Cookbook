7fb4d6548922a33bf19b17d495e4c200f1f1e34a
deetriplett <deetriplett@gmail.com>
2016-07-08 09:04:53 -0700

    Added recipes to $cookbook object

--- cookbook.php
+++ cookbook.php
@@ -2,9 +2,28 @@
 
 include "classes/recipes.php";
 include "classes/render.php";
-include "classes/recipecollection.php";
+include "classes/recipecolection.php";
 include "inc/allrecipes.php";
 
 $cookbook = new RecipeCollection("Monroe Recipes");
-$cookbook-> addRecipe();
+$cookbook-> addRecipe($lemon_chicken);
+$cookbook-> addRecipe($granola_muffins);
+$cookbook-> addRecipe($belgian_waffles);
+$cookbook-> addRecipe($pepper_casserole);
+$cookbook-> addRecipe($lasagna);
+$cookbook-> addRecipe($dried_mushroom_ragout);
+$cookbook-> addRecipe($rabbit_catalan);
+$cookbook-> addRecipe($grilled_salmon_with_fennel);
+$cookbook-> addRecipe($pistachio_duck);
+$cookbook-> addRecipe($chili_pork);
+$cookbook-> addRecipe($crab_cakes);
+$cookbook-> addRecipe($beef_medallions);
+$cookbook-> addRecipe($silver_dollar_cakes);
+$cookbook-> addRecipe($french_toast);
+$cookbook-> addRecipe($corn_beef_hash);
+$cookbook-> addRecipe($granola);
+$cookbook-> addRecipe($spicy_omelette);
+$cookbook-> addRecipe($scones);
+
 echo Render::displayRecipe($belgian_waffles);
+
