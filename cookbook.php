7d8c5ba79f84176cc19f90b3301adc1b8fac3212
deetriplett <deetriplett@gmail.com>
2016-07-08 09:02:02 -0700

    Created cookbook object
    add the recipes to our cookbook collection using the add recipe method

--- cookbook.php
+++ cookbook.php
@@ -2,6 +2,9 @@
 
 include "classes/recipes.php";
 include "classes/render.php";
+include "classes/recipecollection.php";
 include "inc/allrecipes.php";
 
+$cookbook = new RecipeCollection("Monroe Recipes");
+$cookbook-> addRecipe();
 echo Render::displayRecipe($belgian_waffles);
